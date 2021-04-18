73b81a8566c989802ba4ab574a838cba39edcb75
Varghese George <54943444+VargheseGeorge01@users.noreply.github.com>
2021-04-18 20:00:17 +0530

    Add files via upload

---
+++ indexedarray.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>array</title>
+</head>
+<body>
+<?php
+$season=array("summer","winter","spring","autumn");
+echo "first method for printing indexed array <br>";
+echo "SEASONS are $season[0],$season[1],$season[2],$season[3]<br>";
+$season[0]="summer";
+$season[1]="winter";
+$season[2]="spring";
+$season[3]="autumn";
+echo "second method<br>";
+echo "SEASONS are $season[0],$season[1],$season[2],$season[3]<br>";
+?>
+</body>
+</html>
