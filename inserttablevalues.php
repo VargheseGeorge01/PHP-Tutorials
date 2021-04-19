7dd926351c10ecd5ef9354c4d8dc9c43246150fd
Varghese George <54943444+VargheseGeorge01@users.noreply.github.com>
2021-04-19 08:32:29 +0530

    Update inserttablevalues.php

--- inserttablevalues.php
+++ inserttablevalues.php
@@ -3,12 +3,15 @@ $dbhost='localhost';
 $dbuser='root';
 $dbpass='';
 $db='XYZ';
+//create connection
 $conn=mysqli_connect($dbhost,$dbuser,$dbpass,$db);
+//checking connection 
 if($conn->connect_error){
 die("connection failed:".$conn->connect_error);
 }
 echo "connected successfully";
 echo "<br>";
+//Inserting values to the table
 $sql='INSERT INTO employee(emp_name,emp_add,emp_salary)VALUES("George","BCC",3500)';
 if(mysqli_query($conn,$sql)){
 echo "data inserted successfully";
