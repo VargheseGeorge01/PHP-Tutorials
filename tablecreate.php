e12e4d030dbaf5ab0b2f8f04f0f90df496457466
Varghese George <54943444+VargheseGeorge01@users.noreply.github.com>
2021-04-19 08:34:23 +0530

    Update tablecreate.php

--- tablecreate.php
+++ tablecreate.php
@@ -3,12 +3,15 @@ $dbhost='localhost';
 $dbuser='root';
 $dbpass='';
 $db='XYZ';
+//Establishing connection 
 $conn=mysqli_connect($dbhost,$dbuser,$dbpass,$db);
+//Connection checking
 if($conn->connect_error){
 die("connection failed : ".$conn->connect_error);
 }
 echo "connected successfully";
 echo "<br>";
+//Table creation
 $sql="CREATE TABLE employee(emp_id INT NOT NULL AUTO_INCREMENT,emp_name VARCHAR(20) NOT NULL,emp_add VARCHAR(15) NOT NULL,emp_salary INT NOT NULL,primary key(emp_id))";
 if(mysqli_query($conn,$sql)){
 echo "Table created successfully";
