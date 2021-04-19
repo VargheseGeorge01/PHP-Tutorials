ecab991923d93820c593562d474b9f5262a2e22e
Varghese George <54943444+VargheseGeorge01@users.noreply.github.com>
2021-04-19 08:36:23 +0530

    Update tabledelete.php

--- tabledelete.php
+++ tabledelete.php
@@ -3,12 +3,15 @@ $dbhost='localhost';
 $dbuser='root';
 $dbpass='';
 $db='XYZ';
+//creating connection 
 $conn=mysqli_connect($dbhost,$dbuser,$dbpass,$db);
+//connection checking 
 if($conn->connect_error){
 die("connection failed:".$conn->connect_error);
 }
 echo "connected";
 echo "<br>";
+//Deleting data from the table
 $sql="DELETE FROM employee WHERE emp_id=2";
 if(mysqli_query($conn,$sql)){
 echo "deleted successfully";
